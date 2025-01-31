fe00d52084b21164d64e67adf9dbb857072adf6c
selo <ahmadseloabadi@gmail.com>
2025-01-31 21:28:37 +0700

    feat : add script

---
+++ Python/2.Fundamental Class/12. Data Visualization with Python Matplotlib for Beginner - Part 1/3.Penambahan Kolom GMV pada Dataset.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import datetime
+
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+
+dataset['gmv']=dataset['item_price']*dataset['quantity']
+print('Ukuran dataset: %d baris dan %d kolom\n'%dataset.shape)
+print('Lima data teratas:')
+print(dataset.head())
+
+# Teori
+# GMV (Gross Merchandise Value) adalah total nilai barang dagangan yang terjual dalam suatu periode sebelum dikurangi dengan diskon, pajak, atau biaya lainnya.
