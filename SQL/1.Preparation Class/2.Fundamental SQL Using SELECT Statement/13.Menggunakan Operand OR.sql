16bb0fdb9cec438b6464807335f64ad3bcad3c35
selo <ahmadseloabadi@gmail.com>
2025-02-02 04:08:03 +0700

    feat : add script

---
+++ SQL/1.Preparation Class/2.Fundamental SQL Using SELECT Statement/13.Menggunakan Operand OR.sql
@@ -0,0 +1,4 @@
+SELECT * FROM ms_produk WHERE nama_produk = 'Gantungan Kunci DQLab' OR nama_produk ='Tas Travel Organizer DQLab' OR nama_produk = 'Flashdisk DQLab 64 GB';
+
+-- Tugas:
+-- Tambahkan nama_produk 'Flashdisk DQLab 64 GB' ke dalam tabel. Jika berjalan dengan lancar, maka hasilnya terlihat sebagai berikut.
---
+++ SQL/1.Preparation Class/2.Fundamental SQL Using SELECT Statement/14.Filter untuk Angka.sql
@@ -0,0 +1,4 @@
+SELECT * FROM ms_produk WHERE harga > 50000;
+
+-- Tugas:
+-- Tampilkan informasi dengan harga diatas 50000. Jika berjalan dengan lancar, maka akan mendapatkan hasil berikut.
