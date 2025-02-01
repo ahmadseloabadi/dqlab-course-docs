46c705fe525671ef744bb5d8b93bdb6004fda9eb
selo <ahmadseloabadi@gmail.com>
2025-02-02 04:07:58 +0700

    feat : add script

---
+++ SQL/1.Preparation Class/2.Fundamental SQL Using SELECT Statement/11.Prefix dengan Alias Tabel.sql
@@ -0,0 +1,4 @@
+SELECT t2.nama_produk , t2.harga FROM ms_produk  t2
+
+-- Tugas:
+-- Gantilah perintah pada code editor dengan nama alias t2 - tanpa menggunakan keyword AS - untuk tabel ms_produk dan menampilkan kolom nama_produk dan harga, lengkap dengan prefix alias.
---
+++ SQL/1.Preparation Class/2.Fundamental SQL Using SELECT Statement/12.Menggunakan WHERE.sql
@@ -0,0 +1,4 @@
+SELECT * FROM ms_produk WHERE nama_produk = 'Tas Travel Organizer DQLab';
+
+-- Tugas:
+-- Cobalah ubah perintah SELECT pada code editor untuk mengeluarkan data dengan nama_produk bernilai 'Tas Travel Organizer DQLab'. Jika semua berjalan dengan lancar, maka hasilnya akan terlihat sebagai berikut.
