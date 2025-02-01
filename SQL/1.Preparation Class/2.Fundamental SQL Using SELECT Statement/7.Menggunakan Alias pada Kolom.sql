3f0268726a2285831a4326ceb36d87d31479638c
selo <ahmadseloabadi@gmail.com>
2025-02-02 04:07:47 +0700

    feat : add script

---
+++ SQL/1.Preparation Class/2.Fundamental SQL Using SELECT Statement/7.Menggunakan Alias pada Kolom.sql
@@ -0,0 +1,7 @@
+SELECT no_urut AS nomor,nama_produk AS nama FROM ms_produk
+
+-- Tugas:
+-- Coba ubah perintah SELECT di atas untuk mengubah nama kolom dengan details berikut:
+
+-- no_urut menjadi nomor.
+-- nama_produk menjadi nama.
---
+++ SQL/1.Preparation Class/2.Fundamental SQL Using SELECT Statement/8.Menghilangkan Keyword 'AS'.sql
@@ -0,0 +1,7 @@
+SELECT no_urut nomor,nama_produk nama FROM ms_produk
+
+-- Tugas:
+-- Ubah nama kolom dari tabel ms_produk tanpa menggunakan Alias:
+
+-- no_urut menjadi nomor.
+-- nama_produk menjadi nama.
