3a36f46a961c24bc64252ef99a9c12739f6def21
selo <ahmadseloabadi@gmail.com>
2025-02-02 04:07:53 +0700

    feat : add script

---
+++ SQL/1.Preparation Class/2.Fundamental SQL Using SELECT Statement/10.Menggunakan Alias pada Tabel.sql
@@ -0,0 +1,6 @@
+SELECT * FROM ms_produk t2;
+
+-- Tugas:
+-- Ganti nama tabel ms_produk menjadi t2 dan tampilkan seluruh isinya tanpa menggunakan keyword AS.
+
+ 
---
+++ SQL/1.Preparation Class/2.Fundamental SQL Using SELECT Statement/9.Menggabungkan Prefix dan Alias.sql
@@ -0,0 +1,4 @@
+SELECT ms_produk.harga AS harga_jual FROM ms_produk
+
+-- Tugas:
+-- Tampilkan kolom harga dari tabel ms_produk dengan nama alias harga_jual lengkap dengan prefix.
