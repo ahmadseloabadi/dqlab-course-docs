7d9b8bf8b3b39b2a98d1edafebed82705e43e131
selo <ahmadseloabadi@gmail.com>
2025-02-02 04:07:40 +0700

    feat : add script

---
+++ SQL/1.Preparation Class/2.Fundamental SQL Using SELECT Statement/5.Penggunaan SELECT DISTINCT statement.sql
@@ -0,0 +1,4 @@
+SELECT DISTINCT nama_customer ,alamat FROM ms_pelanggan;
+
+-- Tugas:
+-- Tampilkan nama_customer dan alamat dari tabel ms_pelanggan dan hilangkan data duplikat. Jika benar, maka Live Code Editor akan menampilkan tabel seperti berikut pada console.
