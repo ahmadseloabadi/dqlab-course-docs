3ee82d5ce2927c8f6d9ff91815fe8d6481c19da1
selo <ahmadseloabadi@gmail.com>
2025-02-02 04:07:34 +0700

    feat : add script

---
+++ SQL/1.Preparation Class/2.Fundamental SQL Using SELECT Statement/3.Mengambil Lebih dari Satu Kolom dari Tabel.sql
@@ -0,0 +1,4 @@
+SELECT nama_produk,harga FROM ms_produk;
+
+-- Tugas:
+-- Sekarang gantilah perintah SELECT di code editor untuk menampilkan nama_produk dan harga dari tabel yang sama. Ingat untuk memisahkan setiap kolom dengan comma (,).
---
+++ SQL/1.Preparation Class/2.Fundamental SQL Using SELECT Statement/4.Membatasi Pengambilan Jumlah Row Data.sql
@@ -0,0 +1,4 @@
+SELECT nama_produk,harga FROM ms_produk LIMIT 5;
+
+-- Tugas:
+-- Ambillah lima data teratas dari kolom nama_produk dan harga.
