0644bb57048c052b1170e58e6e925598d56c59c3
selo <ahmadseloabadi@gmail.com>
2025-02-02 03:47:29 +0700

    feat : add image

---
+++ SQL/1.Preparation Class/2.Fundamental SQL Using SELECT Statement/1.Mengambil Seluruh Kolom dalam suatu Tabel.sql
@@ -0,0 +1,14 @@
+SELECT * FROM ms_produk
+
+-- Teori
+-- “Untuk mengakses data di database, kita dapat menggunakan SELECT statement. Pada SELECT statement kita menyatakan kolom - kolom mana saja yang ingin kita tampilkan dari suatu tabel di database. SELECT statement tidak berdiri sendiri. Setelah menyatakan kolom - kolom yang ingin ditampilkan, kita melanjutkan dengan FROM. Di FROM inilah kita menyatakan dari tabel mana data yang ingin kita tampilkan berada. SELECT… FROM… adalah statement paling sederhana di SQL, dan merupakan bagian utama dari query. Kita tidak bisa meng-query data tanpa menggunakan statement ini,” jelas Senja.
+
+-- Senja juga menunjukkan padaku Query dasar dan sederhana perintah SELECT yang berfungsi untuk menampilkan seluruh kolom, sebagai berikut:
+
+-- Kata awal, yaitu SELECT digunakan untuk menginformasikan kepada sistem bahwa kita ingin mengambil data. 
+-- Tanda * (bintang) artinya seluruh kolom perlu diambil dari tabel yang dirujuk. Tanda ini sering juga disebut sebagai wildcard.
+-- FROM [NAMA_TABLE], artinya table yang akan diambil datanya.
+-- Tanda ; (titik koma) adalah tanda yang menyatakan akhir dari perintah SELECT atau SQL lain.
+ 
+
+-- Senja mengajak aku untuk langsung mempraktekkan perintah SQL SELECT untuk menampilkan data pada tabel yang bernama ms_produk. 
