971b13c74cd0e54424cc6f8d34a764ea5109be6a
selo <ahmadseloabadi@gmail.com>
2025-02-02 04:08:07 +0700

    feat : add script

---
+++ SQL/1.Preparation Class/2.Fundamental SQL Using SELECT Statement/15.Menggunakan Operand AND.sql
@@ -0,0 +1,4 @@
+SELECT * FROM ms_produk WHERE nama_produk = 'Gantungan Kunci DQLab' AND harga < 50000;
+
+-- Tugas:
+-- Cobalah ganti kondisi contoh di atas dimana harga menjadi lebih kecil dari 50000, dengan nama_produk yang sama.
