d8b7d5ef0f1199e135fec242d545c5c557430722
selo <ahmadseloabadi@gmail.com>
2025-02-04 01:54:03 +0700

    feat : add script

---
+++ Python/2.Fundamental Class/19.Exploratory Data Analysis with Python for Beginner/4.Tugas Praktek.py
@@ -0,0 +1,7 @@
+import pandas as pd
+order_df = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/order.csv")
+print(order_df.head(10))
+
+# Tugas
+# Cobalah untuk check bagaimana contoh data dari dataframe tersebut dengan fungsi head dengan limit 10 baris!
+
