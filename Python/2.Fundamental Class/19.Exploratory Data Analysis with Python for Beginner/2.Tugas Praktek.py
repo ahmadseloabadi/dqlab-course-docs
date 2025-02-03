5024ed71f0687869148f173230c8cc031247dacd
selo <ahmadseloabadi@gmail.com>
2025-02-04 01:53:57 +0700

    feat : add script

---
+++ Python/2.Fundamental Class/19.Exploratory Data Analysis with Python for Beginner/2.Tugas Praktek.py
@@ -0,0 +1,6 @@
+import pandas as pd
+order_df=pd.read_csv("https://storage.googleapis.com/dqlab-dataset/order.csv")
+
+# tugas
+# Cobalah untuk mengimport dataset marketplace ABC dari order.csv dan disimpan ke dalam dataframe bernama order_df.
+
