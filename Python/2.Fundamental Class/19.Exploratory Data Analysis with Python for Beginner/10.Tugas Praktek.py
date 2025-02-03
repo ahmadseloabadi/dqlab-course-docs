a37ef9de3e79f45078536c913df082af3f98c0a7
selo <ahmadseloabadi@gmail.com>
2025-02-04 01:54:32 +0700

    feat : add script

---
+++ Python/2.Fundamental Class/19.Exploratory Data Analysis with Python for Beginner/10.Tugas Praktek.py
@@ -0,0 +1,9 @@
+import pandas as pd
+order_df = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/order.csv")
+# Hitung rata rata dari price per payment_type
+rata_rata = order_df["price"].groupby(order_df["payment_type"]).mean()
+print(rata_rata)
+
+# Tugas
+# Cobalah untuk mencari rata rata dari price per payment_type dari dataset order_df ("https://storage.googleapis.com/dqlab-dataset/order.csv")!
+
