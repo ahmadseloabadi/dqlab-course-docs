15317e5a5ba41a0e2600302663dbd0e374275899
selo <ahmadseloabadi@gmail.com>
2025-02-04 01:54:42 +0700

    feat : add script

---
+++ Python/2.Fundamental Class/20.Data Quality with Python for Beginner/1.Importing Data.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import io
+import pandas_profiling
+retail_raw=pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced_data_quality.csv')
+
+# Tugas 
+# Sebagai langkah pertama yang harus dilakukan adalah inisialisasi Library dan mengimport dataset tersebut ke dalam Python menggunakan library Pandas dan diassign sebagai retail_raw.
+
+# Library yang perlu diimport adalah: (1) pandas, (2) numpy, (3) io, dan (4) pandas_profiling. Untuk dua libray yang pertama importlah sebagai aliasnya.
+
+# Datasetnya tersedia di: 'https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced_data_quality.csv'.
