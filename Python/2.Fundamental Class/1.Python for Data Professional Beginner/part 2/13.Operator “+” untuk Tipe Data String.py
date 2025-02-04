7efc7803494fc03eaaae608501e42126a74b03fd
selo <ahmadseloabadi@gmail.com>
2025-02-04 22:48:57 +0700

    feat : add script

---
+++ Python/2.Fundamental Class/1.Python for Data Professional Beginner/part 2/13.Operator “+” untuk Tipe Data String.py
@@ -0,0 +1,13 @@
+nama_depan = 'John'
+nama_belakang = 'Doee'
+nama_lengkap = nama_depan + " " + nama_belakang
+print(nama_lengkap)
+umur = '27 tahun'
+alamat = 'Jl. Anggrek No. 100'
+nama_umur_alamat = 'Hi, saya ' + nama_lengkap + 'umur' + umur + ', tinggal di' + alamat + '.'
+print(nama_umur_alamat)
+
+# Teori
+# Setelah aku mempelajari String Manipulation, sekarang aku beralih ke Operator +. Operator + pada dua string akan secara otomatis menggabungkan kedua string tersebut. Operator + juga dapat digunakan untuk menambahkan beberapa string secara bersamaan.
+
+# Aku mengikuti contoh yang diberikan dengan mengetik syntax pada live code editor untuk memahami bagaimana Operation + bekerja pada Python.
