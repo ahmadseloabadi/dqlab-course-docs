94a33d209d667360cae4d10369696fe4fd90d532
selo <ahmadseloabadi@gmail.com>
2025-02-04 23:41:49 +0700

    feat : add script

---
+++ Python/2.Fundamental Class/1.Python for Data Professional Beginner/part 1/24.Python for loops – Part 3.py
@@ -0,0 +1,8 @@
+list_daerah = ['Malang', 'Palembang', 'Medan']
+list_buah = ['Apel', 'Duku', 'Jeruk']
+for nama_daerah in list_daerah:
+    for nama_buah in  list_buah:
+        print(nama_buah+" "+nama_daerah)
+
+# Teori
+# Ternyata, aku belajar bahwa ada istilah nested loops, yaitu pengulangan bersarang. Dengan nested loops, aku dapat mengkombinasikan (menambahkan) struktur pengulangan lain di dalamnya.  
