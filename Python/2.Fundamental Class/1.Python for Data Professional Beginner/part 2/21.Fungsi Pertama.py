334997fa747f6fb07cc745f6aeff5d0ec2c59df5
selo <ahmadseloabadi@gmail.com>
2025-02-04 22:49:22 +0700

    feat : add script

---
+++ Python/2.Fundamental Class/1.Python for Data Professional Beginner/part 2/21.Fungsi Pertama.py
@@ -0,0 +1,10 @@
+# Definisikan fungsi
+def contoh_fungsi():
+    print('Halo Dunia')
+    print('Aku sedang belajar bahasa Python')
+# Panggil fungsi yang telah didefinisikan
+contoh_fungsi()
+
+# Teori
+# contoh_fungsi merupakan nama dari fungsi yang aku deklarasikan dan statemen-statemen di dalamnya disebut sebagai isi (body) dari fungsi.
+
