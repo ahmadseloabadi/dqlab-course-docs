28c421b5010937ba1c52fe0284dd3c80d0e708c2
selo <ahmadseloabadi@gmail.com>
2025-02-04 23:40:59 +0700

    feat : add script

---
+++ Python/2.Fundamental Class/1.Python for Data Professional Beginner/part 1/6.Sequence Type – Part 1.py
@@ -0,0 +1,23 @@
+contoh_list = [ 1, 'dua', 3, 4.0, 5]
+print(contoh_list[0])
+print(contoh_list[3])
+contoh_list = [1, 'dua', 3, 4.0, 5]
+contoh_list[3] = 'empat'
+print(contoh_list[3])
+
+# Teori
+# Tipe data ini digunakan untuk menampung sekumpulan data secara terorganisir.
+# Bentuk dari tipe data sequence ini adalah List dan Tuple.
+# Pada part 1 ini, aku akan mempelajari tipe data list terlebih dahulu.
+
+# Tipe data list diawali dengan tanda kurung siku buka ( [ ), memisahkan setiap elemen di dalamnya dengan tanda koma ( , ) dan ditutup dengan kurung siku tutup ( ] ).
+
+# Tugas:
+# Aku diberikan tugas untuk menerapkan variasi tipe data list dengan mengikuti petunjuk yang diberikan Senja. Berikut petunjuknya:
+
+# Petunjuk 1: Input data 1, 'dua', 3, 4.0, 5 ke dalam contoh_list
+# Petunjuk 2: Ambil Elemen pertama dari contoh_list untuk menampilkan output 1 menggunakan print statement
+# Petunjuk 3: Ambil Elemen ke empat dari contoh_list untuk menampilkan output 4.0 menggunakan print statement
+# Petunjuk 4: Input data 1, 'dua', 3, 4.0, 5 ke dalam contoh_list
+# Petunjuk 5: Ubah Elemen keempat dalam contoh_list menjadi 'empat'
+# Petunjuk 6: Tampilkan output elemen keempat yang telah diubah tersebut menggunakan print statement
---
+++ Python/2.Fundamental Class/1.Python for Data Professional Beginner/part 1/7.Sequence Type – Part 2.py
@@ -0,0 +1,18 @@
+contoh_tuple = ('januari','februari','maret','april')
+print(contoh_tuple[0])
+contoh_tuple = ('januari','februari','maret','april')
+contoh_tuple[0] = 'Desember'
+
+# Teori
+# Setelah mempelajari tipe data list, aku mempelajari tipe data tuple. Tipe data tuple juga berfungsi untuk menampung sekumpulan data. Tipe data ini diawali dengan tanda kurung buka ( ( ), memisahkan setiap elemen di dalamnya dengan tanda koma ( , ) dan ditutup dengan tanda kurung tutup ( ) ).
+# Berbeda dengan tipe data list, tipe data tuple bersifat immutable yang berarti elemen pada tipe data tuple tidak dapat diubah setelah proses pendeklarasiannya.
+
+# Tugas:
+# Sekarang aku diberikan tugas untuk menerapkan variasi tipe data tuple dengan mengikuti petunjuk yang diberikan Senja:
+
+# Petunjuk 1: Input data Januari sampai dengan April ke dalam contoh_tuple
+# Petunjuk 2: Ambil Elemen pertama dari contoh_tuple untuk menampilkan output 1 menggunakan print statement
+# Petunjuk 3: Input kembali data Januari sampai dengan April ke dalam contoh_tuple
+# Petunjuk 4: Ubah Elemen pertama dalam contoh_tuple menjadi 'Desember'
+
+# Ketika dijlankan ini akan menghasilkan error yaitu TypeError, silakan kamu submit dengan adanya error dari output pada Petunjuk 4 tersebut. 
