78250cd71c819761618b725e231a98e3559cad30
selo <ahmadseloabadi@gmail.com>
2025-02-04 22:49:16 +0700

    feat : add script

---
+++ Python/2.Fundamental Class/1.Python for Data Professional Beginner/part 2/18.Menentukan String Apakah Diawali atau Diakhiri oleh Sub-string.py
@@ -0,0 +1,13 @@
+# Fitur .startswith()
+print(">>> Fitur .startswith()")
+teks = "Apel malang adalah apel termanis dibanding apel-apel lainnya"
+print(teks.startswith("Apel"))
+print(teks.startswith("apel"))
+# Fitur .endswith()
+print(">>> Fitur .endswith()")
+print(teks.endswith("lainnya"))
+print(teks.endswith("apel"))
+
+# Teori
+# Pada bagian ini, aku akan mempelajari bagaimana menentukan apakah suatu string diawali atau diakhiri dengan suatu substring (teks) tertentu.
+
