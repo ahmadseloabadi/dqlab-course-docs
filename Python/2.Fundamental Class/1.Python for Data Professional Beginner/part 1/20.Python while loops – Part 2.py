7837756f8e397957f89dcb2ec0218e563dae589c
selo <ahmadseloabadi@gmail.com>
2025-02-04 23:41:40 +0700

    feat : add script

---
+++ Python/2.Fundamental Class/1.Python for Data Professional Beginner/part 1/20.Python while loops – Part 2.py
@@ -0,0 +1,18 @@
+tagihan = [50000, 75000, -150000, 125000, 300000, -50000, 200000]
+i = 0
+jumlah_tagihan = len(tagihan)
+total_tagihan = 0
+while i < jumlah_tagihan:
+    # jika terdapat tagihan ke-i yang bernilai minus (di bawah nol),
+    # pengulangan akan dihentikan
+    if tagihan[i] < 0:
+        total_tagihan = -1
+        print("terdapat angka minus dalam tagihan, perhitungan dihentikan!")
+        break
+    total_tagihan += tagihan[i]
+    i += 1
+print(total_tagihan)
+
+# Teori
+# Dalam sebuah struktur kontrol pengulangan, aku dapat menggunakan perintah break untuk keluar dari struktur pengulangan dan perintah continue untuk melanjutkan proses pengulangan berikutnya. 
+
