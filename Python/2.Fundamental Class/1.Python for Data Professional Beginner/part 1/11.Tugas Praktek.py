412c521c2b520c06b4032108d7aba874b19ca092
selo <ahmadseloabadi@gmail.com>
2025-02-04 23:41:12 +0700

    feat : add script

---
+++ Python/2.Fundamental Class/1.Python for Data Professional Beginner/part 1/11.Tugas Praktek.py
@@ -0,0 +1,8 @@
+sepatu = {"nama": "Sepatu Niko", "harga": 150000, "diskon": 30000} 
+baju = {"nama": "Baju Unikloh", "harga": 80000, "diskon": 8000} 
+celana = {"nama": "Celana Lepis", "harga": 200000, "diskon": 60000} 
+daftar_belanja = [sepatu,baju, celana]
+
+
+# Tugas
+# Setelah berhasil merepresentasikan setiap barang ke dalam tipe data dictionary dengan variabel nama, harga, dan diskon, langkahku selanjutnya adalah: mendeklarasikan list dengan nama daftar_belanja yang berisi data sepatu, baju, dan celana.
