ffcc61650aaf070a15fb0f8c68390de9234c004b
selo <ahmadseloabadi@gmail.com>
2025-02-04 23:41:18 +0700

    feat : add script

---
+++ Python/2.Fundamental Class/1.Python for Data Professional Beginner/part 1/13.Nilai Prioritas Operator dalam Python – Part 1.py
@@ -0,0 +1,19 @@
+# Kode awal
+total_harga = 150000
+potongan_harga = 0.3
+pajak = 0.1 # pajak dalam persen ~ 10%
+harga_bayar = 1 - potongan_harga # baris pertama
+harga_bayar *= total_harga # baris kedua
+pajak_bayar = pajak * harga_bayar # baris ketiga
+harga_bayar += pajak_bayar # baris ke-4
+print("Kode awal - harga_bayar=", harga_bayar)
+# Penyederhanaan baris kode dengan menerapkan prioritas operator
+total_harga = 150000
+potongan_harga = 0.3
+pajak = 0.1 # pajak dalam persen ~ 10%
+harga_bayar = (1 - potongan_harga) * total_harga #baris pertama 
+harga_bayar += harga_bayar * pajak # baris kedua
+print("Penyederhanaan kode - harga_bayar=", harga_bayar)
+
+# Tugas:
+# Aku diminta Senja untuk menghitung harga yang harus dibayarkan menggunakan barang senilai 150,000, dengan diskon 30% dan pajak 10%, menggunakan cara yang aku gunakan awal dan cara lebih singkat yang diajarkan Senja. 
