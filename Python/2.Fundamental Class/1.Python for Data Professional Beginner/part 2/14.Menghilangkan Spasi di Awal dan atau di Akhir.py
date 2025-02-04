3277e56bf8b1869f316dc2d4ccb90ac9c1b8fa74
selo <ahmadseloabadi@gmail.com>
2025-02-04 22:49:04 +0700

    feat : add script

---
+++ Python/2.Fundamental Class/1.Python for Data Professional Beginner/part 2/14.Menghilangkan Spasi di Awal dan atau di Akhir.py
@@ -0,0 +1,19 @@
+# Fitur .strip()
+print(">>> Fitur .strip()")
+kata_sambutan = ' halo, selamat siang! '
+kata_sambutan = kata_sambutan.strip()
+print(kata_sambutan)
+# Fitur .lstrip()
+print(">>> Fitur .lstrip()")
+kata_sambutan = ' halo, selamat siang! '
+kata_sambutan = kata_sambutan.lstrip()
+print(kata_sambutan)
+# Fitur .rstrip()
+print(">>> Fitur .rstrip()")
+kata_sambutan = ' halo, selamat siang! '
+kata_sambutan = kata_sambutan.rstrip()
+print(kata_sambutan)
+
+# Teori
+# Sekarang aku akan mempelajari bagaimana menghilangkan kelebihan spasi yang dimiliki di awal dan/atau di akhir variabel string. Pertama, aku melihat fitur yang diberikan pada tabel contoh.
+
---
+++ Python/2.Fundamental Class/1.Python for Data Professional Beginner/part 2/15.Merubah Caps pada String.py
@@ -0,0 +1,18 @@
+# Fitur .capitalize()
+print(">>> Fitur .capitalize()")
+judul_buku = 'belajar bahasa Python'
+print(judul_buku.capitalize())
+# Fitur .lower()
+print(">>> Fitur .lower()")
+judul_buku = 'Belajar Bahasa PYTHON.'
+print(judul_buku.lower())
+# Fitur .upper()
+print(">>> Fitur .upper()")
+judul_buku = 'Belajar Bahasa PYTHON.'
+print(judul_buku.upper())
+
+# Teori
+# Pada bagian ini, aku akan mempelajari cara merubah caps (penggunaan huruf besar dan kecil). Jika diawal kalimat pada suatu string yang dimiliki belum berupa huruf kapital, maka dengan menggunakan fitur .capitalize() kita dengan mudah mengubah string tersebut menjadi kalimat yang benar secara bahasa.
+
+# Tugas:
+# Aku mengikuti contoh yang diberikan dengan mengetik syntax pada live code editor untuk memahami bagaimana fitur .capitalize(), .lower(), dan .upper()
