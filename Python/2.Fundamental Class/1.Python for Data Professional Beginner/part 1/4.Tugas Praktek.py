52bdf1c1167dee8e92a54e4158796c79152b9531
selo <ahmadseloabadi@gmail.com>
2025-02-04 23:40:26 +0700

    feat : add script

---
+++ Python/2.Fundamental Class/1.Python for Data Professional Beginner/part 1/4.Tugas Praktek.py
@@ -0,0 +1,6 @@
+bilangan1 = 20
+bilangan2 = 10
+print(bilangan1 - bilangan2)
+
+# Tugas:
+# Deklarasi variable bilangan1 dengan 20, dan bilangan2 dengan 10 dan tampilkan hasil pengurangan bilangan1 & bilangan 2.
