42cd5449bbe1eef58ba28ca3e8cb7be1ed262d8f
selo <ahmadseloabadi@gmail.com>
2025-02-04 23:41:05 +0700

    feat : add script

---
+++ Python/2.Fundamental Class/1.Python for Data Professional Beginner/part 1/8.Set Type.py
@@ -0,0 +1,22 @@
+contoh_list = ['Dewi', 'Budi', 'Cici', 'Linda', 'Cici'] 
+print(contoh_list)
+contoh_set = {'Dewi', 'Budi', 'Cici', 'Linda', 'Cici'}
+print(contoh_set)
+contoh_frozen_set = ({'Dewi', 'Budi', 'Cici', 'Linda', 'Cici'})
+print(contoh_frozen_set)
+
+
+# Teori
+# Set Type
+# Serupa dengan tipe data sequence, tipe data set digunakan untuk menampung sekumpulan data dengan tipe lainnya. Terdapat dua jenis dari tipe data set yaitu, set dan frozenset.
+
+# Tipe data set diawali dengan tanda kurung buka kurawal ( { ), memisahkan setiap elemen di dalamnya dengan tanda koma ( , ) dan ditutup dengan tanda kurung tutup ( } ). Namun berbeda dengan tipe data sequence, seperti list, tipe data objek tidak mengizinkan adanya elemen dengan nilai yang sama dan tidak mempedulikan urutan dari elemen.
+
+# Bagaimana dengan tipe data frozenset? Tipe data frozenset sebenarnya hanya merupakan set yang bersifat immutable, yang artinya setiap elemen di dalam frozenset tidak dapat diubah setelah proses deklarasinya. Untuk membuat tipe data frozenset,
+
+# Tugas:
+# Sekarang aku diberikan tugas untuk menerapkan variasi set dan frozenset oleh Senja:
+
+# Tugas 1:Input data Dewi, Budi, Cici, Linda, Cici kedalam tipe data list dan tampilkan hasilnya
+# Tugas 2: Input data Dewi, Budi, Cici, Linda, Cici kedalam tipe data set dan tampilkan hasilnya
+# Tugas 3: Input data Dewi, Budi, Cici, Linda, Cici kedalam tipe data frozenset dan tampilkan hasilnya
---
+++ Python/2.Fundamental Class/1.Python for Data Professional Beginner/part 1/9.Mapping Type.py
@@ -0,0 +1,14 @@
+person = {'nama':'John Doe','pekerjaan':'Programmer'}
+print(person['nama'])
+print(person['pekerjaan'])
+
+
+# Teori
+# Mapping Type
+# tipe data mapping dapat digunakan untuk memetakan sebuah nilai ke nilai lainnya. Dalam Python, tipe data mapping disebut dengan istilah dictionary. Tipe data dictionary dapat dideklarasikan dengan diawali oleh tanda kurung buka kurawal ( { ), memisahkan setiap elemen di dalamnya dengan tanda koma ( , ) dan ditutup dengan tanda kurung kurawal ( } ). Setiap elemen pada tipe data dictionary dideklarasikan dengan format:
+
+# "kunci" : "nilai"
+# Hal inilah yang membedakan tipe data mapping dengan tipe data set.  
+
+# Tugas:
+# Menggunakan tipe data mapping, aku diminta Senja untuk menampilkan nama & pekerjaan John Doe, seorang Programmer.
