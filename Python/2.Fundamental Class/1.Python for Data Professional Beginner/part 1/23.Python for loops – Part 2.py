284b6a8718e49634ded6007b838517bb383c70e7
selo <ahmadseloabadi@gmail.com>
2025-02-04 23:41:45 +0700

    feat : add script

---
+++ Python/2.Fundamental Class/1.Python for Data Professional Beginner/part 1/23.Python for loops – Part 2.py
@@ -0,0 +1,26 @@
+list_tagihan = [50000, 75000, -150000, 125000, 300000, -50000, 200000]
+
+# For loops with break
+print("For loops with break")
+total_tagihan_break = 0
+for tagihan in list_tagihan:
+    if tagihan < 0:
+        print("Terdapat angka minus dalam tagihan, perhitungan dihentikan!")
+        break
+    total_tagihan_break += tagihan
+print("Total tagihan %d." % total_tagihan_break)
+print()
+
+# For loops with continue
+print("For loops with continue")
+total_tagihan_continue = 0
+for tagihan  in list_tagihan:
+    if tagihan < 0:
+        print("Terdapat angka minus dalam tagihan, tagihan %d dilewati!" % tagihan)
+        continue
+    total_tagihan_continue += tagihan
+print("Total tagihan %d." % total_tagihan_continue)
+
+# Teori
+# Serupa dengan struktur pengulangan while, aku juga dapat memanfaatkan statement break dan continue di dalamnya. Aku mencoba potongan kode di bawah menggunakan live code editor untuk penggunaan statement break.
+
