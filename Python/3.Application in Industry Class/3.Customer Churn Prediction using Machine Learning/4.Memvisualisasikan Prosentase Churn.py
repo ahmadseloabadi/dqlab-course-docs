35fb12bdb2aa5712c1b9737201f5887145217567
selo <ahmadseloabadi@gmail.com>
2025-02-08 23:01:41 +0700

    feat : add script

---
+++ Python/3.Application in Industry Class/3.Customer Churn Prediction using Machine Learning/4.Memvisualisasikan Prosentase Churn.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df_load = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/dqlab_telco_final.csv')
+
+#Your codes here
+fig = plt.figure()
+ax = fig.add_axes([0,0,1,1])
+ax.axis('equal')
+labels = ['Yes','No']
+churn = df_load.Churn.value_counts()
+ax.pie(churn, labels=labels, autopct='%.0f%%')
+plt.show()
+
+# Tugas
+# Kita ingin melihat visualisasi data secara univariat terkait prosentase data churn dari pelanggan. Gunakan fungsi value_counts() untuk menghitung banyaknya unik dari sebuah kolom, pie() untuk membuat pie chart
