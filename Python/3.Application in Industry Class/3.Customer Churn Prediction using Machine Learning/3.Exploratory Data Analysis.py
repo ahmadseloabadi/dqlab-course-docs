440de70bfc13c13f55e13c4d139107dfde573d93
selo <ahmadseloabadi@gmail.com>
2025-02-08 23:01:36 +0700

    feat : add script

---
+++ Python/3.Application in Industry Class/3.Customer Churn Prediction using Machine Learning/3.Exploratory Data Analysis.py
@@ -0,0 +1,15 @@
+#import matplotlib dan seaborn
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Tugas
+# Exploratory Data Analysis memungkinkan analyst memahami isi data yang digunakan, mulai dari distribusi, frekuensi, korelasi dan lainnya. Pada umumnya EDA dilakukan dengan beberapa cara:
+
+# Univariat Analysis — analisis deskriptif dengan satu variabel.
+# Bivariat Analysis — analisis relasi dengan dua variabel yang biasanya dengan target variabel.
+# Multivariat Analysis — analisis yang menggunakan lebih dari atau sama dengan tiga variabel.
+# Dalam kasus ini, kamu diminta untuk melihat persebaran dari:
+
+# Prosentase persebaran data Churn dan tidaknya dari seluruh data
+# Persebarang data dari variable predictor terhadap label (Churn)
+# Lakukan import matplotlib dan seaborn
