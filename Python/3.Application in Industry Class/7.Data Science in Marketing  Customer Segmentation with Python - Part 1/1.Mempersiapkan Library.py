bec75ef3d779cb1e356381e28b8df94661dccc39
selo <ahmadseloabadi@gmail.com>
2025-02-13 00:48:35 +0700

    feat : add script

---
+++ Python/3.Application in Industry Class/7.Data Science in Marketing  Customer Segmentation with Python - Part 1/1.Mempersiapkan Library.py
@@ -0,0 +1,24 @@
+import pandas as pd  
+import matplotlib.pyplot as plt  
+import seaborn as sns  
+from sklearn.preprocessing import LabelEncoder  
+  
+from kmodes.kmodes import KModes  
+from kmodes.kprototypes import KPrototypes  
+  
+import pickle  
+from pathlib import Path  
+  
+
+# Tugas
+# Masalah ini akan dapat di selesaikan dengan menggunakan bantuan library - library di bawah ini:
+
+# Pandas, digunakan untuk melakukan pemrosesan analisis data
+# Matplotlib, digunakan sebagai dasar untuk melakukan visualisasi data
+# Seaborn, digunakan di atas matplotlib untuk melakukan data visualisasi yang lebih menarik
+# Scikit - Learn, digunakan untuk mempersiapkan data sebelum dilakukan permodelan
+# kmodes, digunakan untuk melakukan permodelan menggunakan algoritma K-Modes dan K-Prototypes.
+# Pickle, digunakan untuk melakukan penyimpanan dari model yang akan di buat.
+# Tugas:
+
+# Lakukan import library yang dibutuhkan pada proyek ini.
