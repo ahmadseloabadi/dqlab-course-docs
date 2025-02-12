e6f3d2d8400e43d191e5cf58c4cb792f1a0c4990
selo <ahmadseloabadi@gmail.com>
2025-02-12 22:14:49 +0700

    feat : add script

---
+++ Python/3.Application in Industry Class/6.Eksplorasi dan Analisis Data COVID-19 Indonesia using Python/23.Kabar Buruk dan Kabar Baik.py
@@ -0,0 +1,10 @@
+True
+
+# Tugas
+# Sebagai penutup analisis dalam proyek ini Anda diminta untuk membuat grafik komparasi antara akumulasi kasus aktif, kasus sembuh, dan kasus meninggal.
+
+# Ada dua pilihan cara yang dapat Anda tempuh untuk membuat grafik tersebut:
+
+# Menggunakan data frame cov_jabar_akumulasi, dengan kolom bertipe datetime diset menjadi index sehingga kita memiliki time series dataframe. Kemudian kita secara langsung dapat melakukan line plot dan memilih warna garis yang sesuai untuk membedakan; atau
+# Menggunakan ax.plot() dan mengisikan kolom-kolom yang masing-masingnya menjadi x dan y dengan data=cov_jabar_akumulasi, dan kemudian menset warna berbeda untuk ketiga line plot.
+# Anda akan diminta untuk menempuh cara nomor 1. Ketik True pada konsol jika Anda setuju!
