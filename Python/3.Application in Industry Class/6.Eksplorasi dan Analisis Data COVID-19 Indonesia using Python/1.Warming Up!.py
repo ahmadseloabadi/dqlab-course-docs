3d7ccc88f3e7752adc656f4f886ad92947f1df4a
selo <ahmadseloabadi@gmail.com>
2025-02-12 22:13:31 +0700

    feat : add script

---
+++ Python/3.Application in Industry Class/6.Eksplorasi dan Analisis Data COVID-19 Indonesia using Python/1.Warming Up!.py
@@ -0,0 +1,10 @@
+bersatulawancovid = ['cuci tangan', 'pakai masker', 'jaga jarak']
+print(bersatulawancovid)
+
+# Tugas
+# Sebagai pemanasan sebelum Anda memulai proyek ini, buatlah sebuah vektor karakter dengan 3 elemen dengan nama bersatulawancovid yang merupakan kunci dasar menghindari COVID-19! Jangan lupa untuk mencetak vektor bersatulawancovid tersebut pada layar konsol!
+
+# Latar Belakang Project
+# COVID-19 merupakan penyakit yang saat ini telah menjadi pandemi secara global. Kondisi menjadi semakin mengkhawatirkan karena hingga detik ini masih belum ditemukan vaksin yang efektif untuk virus penyebab COVID-19. Pemerintah di berbagai negara umumnya dengan sigap membentuk gugus tugas (task force unit) untuk menangani penyebaran COVID-19 di masyarakat, termasuk pemerintah di Indonesia.
+
+# Salah satu bentuk aksi yang dilakukan oleh pemerintah adalah dengan mengumpulkan dan menyediakan data pertumbuhan kasus COVID-19 kepada publik. Data pertumbuhan kasus tersebut tidak jarang juga dilengkapi dengan dasbor dan grafik visualisasi pendukung dengan harapan masyarakat dapat memahami informasi dengan lebih mudah. Sebagai contoh adalah portal covid19.go.id besutan Gugus Tugas Penanganan COVID-19 Nasional dan portal PIKOBAR milik pemerintah Provinsi Jawa Barat. Serta banyak portal data COVID-19 lainnya yang disediakan oleh masing-masing pemerintah daerah.
---
+++ Python/3.Application in Industry Class/6.Eksplorasi dan Analisis Data COVID-19 Indonesia using Python/2.Are You Ready.py
@@ -0,0 +1,6 @@
+True
+
+# Tugas
+# Dalam proyek ini, Anda akan diminta untuk melakukan eksplorasi dan analisis mengenai COVID-19 di Indonesia. Data yang Anda pergunakan diambil langsung dari API (Application Programming Interface) yang tersedia di covid19.go.id. Sehingga dalam proyek ini Anda akan mempelajari teknik mengambil data dari API, teknik mempersiapkan data, serta analisis dan visualisasi data.
+
+# Apakah Anda siap untuk mengerjakan proyek analisis data ini? Jawablah dengan mengetikkan True apabila Anda siap atau False jika Anda belum siap karena belum mencuci tangan.
