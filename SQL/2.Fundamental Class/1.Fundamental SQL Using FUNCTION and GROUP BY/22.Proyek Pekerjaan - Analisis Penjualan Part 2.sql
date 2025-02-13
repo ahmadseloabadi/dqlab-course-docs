22d5c3211af3af3b700e013a7a80724ea94108ef
selo <ahmadseloabadi@gmail.com>
2025-02-13 16:04:02 +0700

    feat : add script

---
+++ SQL/2.Fundamental Class/1.Fundamental SQL Using FUNCTION and GROUP BY/22.Proyek Pekerjaan - Analisis Penjualan Part 2.sql
@@ -0,0 +1,25 @@
+-- 4. Rata - Rata total belanja per kode pelanggan.
+SELECT kode_pelanggan, avg(total) as avg_total 
+FROM tr_penjualan
+GROUP BY kode_pelanggan;
+/* 5. Selain itu,  jangan lupa untuk menambahkan kolom baru 
+dengan nama ‘kategori’ yang mengkategorikan total/revenue ke dalam 
+3 kategori: High: > 300K; Medium: 100K - 300K; Low: <100K. */
+SELECT kode_transaksi,kode_pelanggan,no_urut,kode_produk, nama_produk, qty, total,
+CASE  
+    WHEN total > 300000 THEN 'High'
+    WHEN total < 100000 THEN 'Low'   
+    ELSE 'Medium'  
+END as kategori 
+FROM tr_penjualan;
+
+-- Tugas
+-- (lanjutan ...)
+
+-- Saya mau minta tolong agar kamu melakukan analisis penjualan di suatu store. Adapun laporan yang diminta sebagai berikut:
+
+-- Rata - Rata total belanja per kode pelanggan.
+-- Selain itu,  jangan lupa untuk menambahkan kolom baru dengan nama ‘kategori’ yang mengkategorikan total/revenue ke dalam 3 kategori: High: > 300K; Medium: 100K - 300K; Low: <100K.
+ 
+
+-- Tabel: tr_penjualan
---
+++ SQL/2.Fundamental Class/1.Fundamental SQL Using FUNCTION and GROUP BY/5.Fungsi Skalar Matematika - SQRT( ).sql
@@ -0,0 +1,9 @@
+select StudentID, FirstName, LastName, sqrt(Semester1) as Semester1, Semester2, MarkGrowth from students;
+
+-- Teori
+-- Fungsi SQRT()
+
+-- Syntax: 
+
+-- SELECT SQRT(ColumnName)  
+-- FROM TableName; 
