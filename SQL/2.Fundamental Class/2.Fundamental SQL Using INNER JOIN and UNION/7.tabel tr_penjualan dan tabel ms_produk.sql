c1d6becc71613bd57490e72fdc4776a369ab024a
selo <ahmadseloabadi@gmail.com>
2025-02-13 19:04:49 +0700

    feat : add script

---
+++ SQL/2.Fundamental Class/2.Fundamental SQL Using INNER JOIN and UNION/7.tabel tr_penjualan dan tabel ms_produk.sql
@@ -0,0 +1,8 @@
+select * from tr_penjualan;
+select * from ms_produk;
+
+-- Teori
+-- Tak kusangka penggunaan INNER JOIN ini banyak sekali langkah per langkahnya. Untungnya aku sudah diberi modul oleh Senja, kalau tidak, buku catatanku bisa habis diisi oleh materi ini saja.
+
+-- “Setelah kita menggunakan tabel ms_item_warna dan ms_item_kategori, sekarang mari kita gunakan tabel tr_penjualan dan tabel ms_produk yang ada di-database, sehingga kita bisa lebih paham mengenai penerapan konsep JOIN di real case,
+
---
+++ SQL/2.Fundamental Class/2.Fundamental SQL Using INNER JOIN and UNION/8.Tugas Praktek Menggunakan INNER JOIN (2-3).sql
@@ -0,0 +1,4 @@
+SELECT * FROM tr_penjualan INNER JOIN ms_produk ON tr_penjualan.kode_produk = ms_produk.kode_produk;
+
+-- Tugas
+-- Oke, Aksara. Silakan dipraktikkan pada code editor untuk menggabungkan tabel tr_penjualan dan ms_produk dan menampilkan seluruh kolom dari kedua tabel.”
