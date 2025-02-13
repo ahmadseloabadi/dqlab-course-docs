28a49243bba3232c313ce59ef5b2f371de7ae7fa
selo <ahmadseloabadi@gmail.com>
2025-02-13 16:03:56 +0700

    feat : add script

---
+++ SQL/2.Fundamental Class/1.Fundamental SQL Using FUNCTION and GROUP BY/3.Fungsi Skalar Matematika - FLOOR().sql
@@ -0,0 +1,9 @@
+select StudentID, FirstName, LastName, floor(Semester1) as Semester1, floor(Semester2) as Semester2, MarkGrowth from students;
+
+-- Teori
+-- Fungsi FLOOR()
+
+-- Syntax: 
+
+-- SELECT FLOOR(ColumnName)  
+-- FROM TableName; 
---
+++ SQL/2.Fundamental Class/1.Fundamental SQL Using FUNCTION and GROUP BY/4.Fungsi Skalar Matematika - ROUND().sql
@@ -0,0 +1,9 @@
+select StudentID, FirstName, LastName, round(Semester1, 1) as Semester1, round(Semester2, 0) as Semester2, MarkGrowth from students;
+
+-- Teori
+-- Fungsi ROUND()
+
+-- Syntax: 
+
+-- SELECT ROUND(ColumnName)  
+-- FROM TableName; 
