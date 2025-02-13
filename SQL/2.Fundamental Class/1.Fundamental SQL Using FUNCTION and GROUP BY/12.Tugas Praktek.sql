91028945eb3473d6b9560544b4f4bfa2694e8922
selo <ahmadseloabadi@gmail.com>
2025-02-13 16:04:24 +0700

    feat : add script

---
+++ SQL/2.Fundamental Class/1.Fundamental SQL Using FUNCTION and GROUP BY/12.Tugas Praktek.sql
@@ -0,0 +1,4 @@
+select StudentID, upper(FirstName) as FirstName, lower(LastName) as LastName from students;
+
+-- Tugas:
+-- Gunakan fungsi UPPER() untuk mengubah kolom FirstName menjadi seluruhnya kapital dan gunakan LOWER() untuk mengubah kolom LastName menjadi seluruhnya non-kapital. Gunakan kedua fungsi tersebut dalam satu SELECT-Statement.
---
+++ SQL/2.Fundamental Class/1.Fundamental SQL Using FUNCTION and GROUP BY/13.Fungsi Aggregate - SUM().sql
@@ -0,0 +1,9 @@
+select sum(Semester1) as Total_1, SUM(Semester2) as Total_2 from students;
+
+-- Teori
+-- Fungsi Aggregate SUM()
+
+-- Syntax: 
+
+-- SELECT SUM(ColumnName)  
+-- FROM TableName; 
