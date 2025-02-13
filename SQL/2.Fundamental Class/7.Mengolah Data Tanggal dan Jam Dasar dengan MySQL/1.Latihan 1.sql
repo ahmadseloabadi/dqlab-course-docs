ea69bfc7a6c6c6b9d9a6f870dc37e2e3c59e8a39
selo <ahmadseloabadi@gmail.com>
2025-02-14 00:02:06 +0700

    feat : add script

---
+++ SQL/2.Fundamental Class/7.Mengolah Data Tanggal dan Jam Dasar dengan MySQL/1.Latihan 1.sql
@@ -0,0 +1,5 @@
+SELECT * FROM dqlab_retail
+
+-- Tugas:
+
+-- "Sekarang, coba ketikkan kodenya pada console, bagaimana caranya untuk menampilkan data semua transaksi dari tabel dqlab_retail?"
---
+++ SQL/2.Fundamental Class/7.Mengolah Data Tanggal dan Jam Dasar dengan MySQL/2.Latihan 2.sql
@@ -0,0 +1,5 @@
+SELECT *, (Jumlah * Harga) AS subtotal
+FROM dqlab_retail;
+
+-- Tugas
+-- Sekarang, bagaimana caranya untuk menampilkan data semua transaksi dan menghitung subtotal?"
