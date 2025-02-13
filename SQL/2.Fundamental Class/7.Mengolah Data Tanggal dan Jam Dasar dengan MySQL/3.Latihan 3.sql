c125b083363b6497d6e450f8ff9036db730453ab
selo <ahmadseloabadi@gmail.com>
2025-02-14 00:02:12 +0700

    feat : add script

---
+++ SQL/2.Fundamental Class/7.Mengolah Data Tanggal dan Jam Dasar dengan MySQL/3.Latihan 3.sql
@@ -0,0 +1,5 @@
+SELECT * FROM dqlab_retail
+WHERE tanggal BETWEEN '2020-04-01' AND '2020-04-30';
+
+-- Tugas
+-- "Bagaimana cara untuk menampilkan data transaksi, mulai dari: 1 April 2020 – 30 April 2020?"
---
+++ SQL/2.Fundamental Class/7.Mengolah Data Tanggal dan Jam Dasar dengan MySQL/4.Latihan 4.sql
@@ -0,0 +1,5 @@
+SELECT SUM(Jumlah * Harga) as total
+FROM dqlab_retail;
+
+-- Tugas
+-- "Pertanyaan berikutnya, apa yang kamu lakukan untuk menampilkan data total semua transaksi, Nyi?"
