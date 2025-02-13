3a7a21f5fb5757ba008bbe092467b9c4cc02041f
selo <ahmadseloabadi@gmail.com>
2025-02-13 19:04:31 +0700

    feat : add script

---
+++ SQL/2.Fundamental Class/2.Fundamental SQL Using INNER JOIN and UNION/1.Tugas Praktek.sql
@@ -0,0 +1,5 @@
+select * from ms_item_kategori;
+select * from ms_item_warna;
+
+-- Tugas
+-- Cobalah ketik query pada code editor untuk melihat keseluruhan isi dari kolom tabel ms_item_kategori dan ms_item_warna.
---
+++ SQL/2.Fundamental Class/2.Fundamental SQL Using INNER JOIN and UNION/2.Menggabungkan Tabel dengan Key Columns.sql
@@ -0,0 +1,8 @@
+select * from ms_item_kategori, ms_item_warna where nama_barang = nama_item;
+
+-- Tugas
+-- Saatnya  mempraktekkan penggabungan tabel ms_item_kategori dan ms_item_warna menggunakan key columns.
+
+-- Penjelasan
+-- Dapat dilihat hasil dari penggabungan dua tabel tersebut yaitu berupa tabel baru dengan empat kolom dan enam baris data. Perlu diketahui bahwa penggabungan ini bersifat sementara artinya tabel asli di database tidak mengalami perubahan, dan tabel baru hasil penggabungan ini juga tidak serta merta tersimpan di database.  
+
