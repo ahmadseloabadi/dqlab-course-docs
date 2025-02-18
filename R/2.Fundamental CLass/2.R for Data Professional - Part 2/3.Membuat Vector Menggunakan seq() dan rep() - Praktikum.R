08be7d06f4005858beb00d6d3c760702bbfb4217
selo <ahmadseloabadi@gmail.com>
2025-02-19 01:15:11 +0700

    feat : add script

---
+++ R/2.Fundamental CLass/2.R for Data Professional - Part 2/3.Membuat Vector Menggunakan seq() dan rep() - Praktikum.R
@@ -0,0 +1,18 @@
+#vector_seq berisi angka 1 - 2 dengan interval 0.2
+vector_seq <- seq(1, 2, 0.2)
+vector_seq
+
+#my_vector berisi "Belajar", "Data Science", "di DQLab"
+my_vector <- c("Belajar", "Data Science", "di DQLab")
+my_vector
+
+#vector_rep berisi perulangan my_vector sebanyak 3 kali
+vector_rep <- rep(my_vector,3)
+vector_rep
+
+# Tugas
+# Berikut instruksi dari Andra :
+
+# Buat vector bernama vector_seq yang berisi data numerik 1 sampai dengan 2 dengan interval 0.2 antar bilangannya.
+# Buat vector bernama my_vector yang berisi data character "Belajar", "Data Science", "di DQLab" secara berurutan.
+# Buat vector bernama vector_rep yang berisi perulangan dari my_vector sebanyak 3 kali.
---
+++ R/2.Fundamental CLass/2.R for Data Professional - Part 2/4.Berapa Jam Kerja Andra Selama 5 Hari Ini - Praktikum.R
@@ -0,0 +1,22 @@
+#Masukkan nama hari dari Senin - Jumat
+nama_hari <- c("Senin", "Selasa", "Rabu", "Kamis", "Jumat")
+
+#Masukkan jam kerja berurutan dari jam kerja di hari senin
+jam_kerja <- c(8,7.5,10,7,7.5)
+
+# Tugas
+# “Betul. Nah, ini adalah jam kerja selama seminggu ini,” kata Andra sambil memberikan daftar jadwal berikut.
+
+# Hari Senin, Andra datang ke kantor tepat waktu.
+
+# Hari Selasa, Andra terlambat setengah jam.
+
+# Hari Rabu, Andra lembur untuk dua 2 jam.
+
+# Hari Kamis, Andra terlambat satu 1 jam.
+
+# Hari Jumat, Andra terlambat setengah jam.
+
+# Notes : 
+
+# Jam kerja normal di DQLab untuk satu hari adalah sebanyak 8 jam.
