c01fd9644d9b1774e258e75004dcd25723b2914a
selo <ahmadseloabadi@gmail.com>
2025-02-18 22:20:45 +0700

    feat : add script

---
+++ R/2.Fundamental CLass/1.R for Data Professional - Part 1/3.Melakukan Assign Value Pada Variable - Praktikum.R
@@ -0,0 +1,13 @@
+#Simpan nilai apel ke dalam variable bernama ‘apel’
+apel <- 5
+  
+#Simpan nilai jeruk ke dalam variable bernama ‘jeruk’
+jeruk <- 7
+
+#Tampilkan nilai dari variable ‘jeruk’
+jeruk
+
+
+# TUgas
+# “Anggap kamu memiliki sebuah keranjang buah berisi 5 apel dan 7 jeruk. Sebagai seorang Data Scientist, kamu ingin menyimpan nilai dari apel dan jeruk itu ke dalam variabel lalu tampilkan nilai dari jeruk."
+
