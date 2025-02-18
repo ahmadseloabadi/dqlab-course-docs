07503d5011f06a3b9b3dfc9d4ac0cd7aa5efda6f
selo <ahmadseloabadi@gmail.com>
2025-02-18 22:20:59 +0700

    feat : add script

---
+++ R/2.Fundamental CLass/1.R for Data Professional - Part 1/7.Apa ya Tipe Data dari Variabel ini - Praktikum.R
@@ -0,0 +1,20 @@
+#buat variable data_numerik_1 yang berisi 7
+data_numerik_1 <- 7
+class(data_numerik_1)
+
+#buat variable data_numerik_2 yang berisi 77.7
+data_numerik_2 <- 77.7
+class(data_numerik_2)
+
+#buat variable data_character yang berisi "Belajar Data Science Sangat Menyenangkan!"
+data_character <- "Belajar Data Science Sangat Menyenangkan!"
+class(data_character)
+
+#buat variable data_logical yang berisi TRUE
+data_logical <- TRUE
+class(data_logical)
+
+
+# TUgas
+# fungsi class(x) dan mengecek tipe data dari variabel - variabel yang sudah dibuat sebelumnya.
+
