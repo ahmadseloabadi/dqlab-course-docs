01eb7c11f7f224655a12df317bf1249795f2da03
selo <ahmadseloabadi@gmail.com>
2025-02-18 22:20:38 +0700

    feat : add script

---
+++ R/2.Fundamental CLass/1.R for Data Professional - Part 1/2.Menggunakan Operator Matematika untuk Menyelesaikan Masalah Sederhana - Praktikum.R
@@ -0,0 +1,19 @@
+#Populasi manusia tahun depan
+7900000000 * 1.01
+
+#Populasi manusia dua tahun mendatang
+7900000000 * 1.01 * 1.01
+
+#Populasi manusia sepuluh tahun mendatang
+7900000000 * 1.01 ^ 10
+
+# TUgas
+# Aku mulai menyimak soal dan instruksi yang Andra berikut padaku sebagai berikut.
+
+# Pada saat ini populasi manusia di dunia adalah sebanyak sekitar 7,900,000,000 jiwa.
+# Pertambahan populasi manusia di dunia setiap tahunnya adalah sekitar 1%
+# Anggap pertambahan populasi manusia di dunia stabil pada angka 1%
+# Pada tahun depan, berarti populasi manusia di dunia adalah kira-kira sebesar 7,900,000,000 * 1.01
+# Lalu pada dua tahun mendatang, populasi manusia di dunia akan menjadi 7,900,000,000 * 1.01 * 1.01
+
+# Pertanyaan : Berapa populasi manusia di dunia pada sepuluh tahun mendatang ya?
