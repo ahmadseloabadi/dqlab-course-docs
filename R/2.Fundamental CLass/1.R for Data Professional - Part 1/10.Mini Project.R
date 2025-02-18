70a2041054fcc540675953bdf6802fa8c66c9830
selo <ahmadseloabadi@gmail.com>
2025-02-18 22:21:06 +0700

    feat : add script

---
+++ R/2.Fundamental CLass/1.R for Data Professional - Part 1/10.Mini Project.R
@@ -0,0 +1,31 @@
+#variable hari_pahlawan
+hari_pahlawan <- as.Date("Nov 10, 2021", "%b %d, %Y")
+ 
+#menampilkan variable hari_pahlawan
+hari_pahlawan
+ 
+#variable radius
+radius <- 15
+ 
+#variable luas_lingkaran
+luas_lingkaran <-  3.14 * radius ^ 2
+ 
+#menampilkan variable luas_lingkaran
+luas_lingkaran
+ 
+#pembulatan ke atas luas_lingkaran
+ceiling(luas_lingkaran)
+ 
+#pembulatan ke bawah luas_lingkaran
+floor(luas_lingkaran)
+
+
+# Tugas
+# Tugas dari Andra:
+
+# Buatlah sebuah variabel bernama hari_pahlawan dan isi dengan "Nov 10, 2021" menggunakan format tanggal yang sudah dipelajari sebelumnya dan tampilkan hasilnya
+# Buatlah sebuah variabel bernama radius dan isi dengan angka 15
+# Dengan menggunakan variabel radius tersebut, hitung luas sebuah lingkaran (pi = 3.14) dan simpan hasilnya ke dalam variabel bernama luas_lingkaran. Notes: Rumus Luas Lingkaran = pi * r ^ 2
+# Tampilkan nilai dari luas_lingkaran
+# Tampilkan pembulatan ke atas dari luas_lingkaran
+# Tampilkan pembulatan ke bawah dari luas_lingkaran
