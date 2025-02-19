41dd7b544cbf2dd60b8fd4a62bd083d927eead2d
selo <ahmadseloabadi@gmail.com>
2025-02-19 17:32:06 +0700

    feat : add script

---
+++ R/2.Fundamental CLass/5.Statistics using R for Data Science/11.Estimasi Nilai Sebaran Data Simpangan Baku.R
@@ -0,0 +1,11 @@
+## Carilah simpangan baku untuk kolom Pendapatan dari variable data_intro
+sd(data_intro$Pendapatan)
+
+# Teori
+# Simpangan baku adalah simpangan data dari nilai rata-ratanya, simpangan baku nama lainnya adalah standard deviasi. Standard deviasi dapat digunakan untuk melihat keakuratan dari hasil estimasi, semakin kecil standard deviasi semakin akurat hasil estimasi.
+
+# sd adalah function yang digunakan untuk mendapatkan nilai simpangan baku dari data.
+
+# Tugas Praktek
+
+# Lengkapi bagian [...1...] pada code editor untuk mengeluarkan hasil simpangan baku dari kolom Pendapatan dari variabel data_intro.
---
+++ R/2.Fundamental CLass/5.Statistics using R for Data Science/12.Analisis Deskriptif Menggunakan Nilai Statistik.R
@@ -0,0 +1,27 @@
+## carilah summary data dari data_intro
+summary(data_intro)
+
+
+# Teori
+# Analisis Deskriptif pada variable data_intro
+# Analisis Deskriptif adalah proses analisa yang digunakan untuk membangun sebuah hipotesis.
+
+# Pada bab ini, analisis deskriptif akan dilakukan pada data sebelumnya dengan tujuan untuk mendapatkan informasi berikut:
+
+# Bagaimana profil pelanggan.
+# Bagaimana gambaran produk.
+# Membangun hipotesis.
+ 
+# Function summary akan menampilkan kesimpulan pada variabel masing-masing. Untuk variabel bertipe character akan menampilkan panjang datanya. Variabel bertipe factor akan menampilkan jumlah data pada masing-masing kelas. Sedangkan untuk variabel bertipe numerik akan memunculkan nilai minimum, Q1,Q2 (median), Q3, mean, dan maximum.
+
+# Pengertian dari masing-masing istilah itu adalah sebagai berikut :
+
+# Minimum adalah nilai observasi terkecil.
+# Kuartil pertama (Q1), yang memotong 25 % dari data terendah.
+# Median (Q2) atau nilai pertengahan.
+# Kuartil ketiga (Q3), yang memotong 25 % dari data tertinggi.
+# Maksimum adalah nilai observasi terbesar.
+
+# Tugas Praktek
+
+# Gantilah bagian […1...] pada code editor untuk  mendapatkan summary dari variable data_intro.
