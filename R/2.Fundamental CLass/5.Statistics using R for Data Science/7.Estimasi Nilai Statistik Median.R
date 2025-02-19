05bb4328e0640569011a76dc9dbef649512b1a26
selo <ahmadseloabadi@gmail.com>
2025-02-19 17:31:56 +0700

    feat : add script

---
+++ R/2.Fundamental CLass/5.Statistics using R for Data Science/7.Estimasi Nilai Statistik Median.R
@@ -0,0 +1,18 @@
+## carilah median untuk kolom Pendapatan dari variable data_intro
+median(data_intro$Pendapatan)
+
+## carilah median untuk  kolom Harga dari variable data_intro
+median(data_intro$Harga)
+
+## carilah median untuk kolom Jumlah dari variable data_intro
+median(data_intro$Jumlah)
+
+## carilah median untuk  kolom Total dari variable data_intro
+median(data_intro$Total)
+
+
+# Teori
+# Berikut penjelasan function diatas :
+
+# median akan menampilkan nilai tengah pada variabel yang diamati.
+# data_intro$Pendapatan, merupakan kolom Pendapatan dari variabel data_intro.
