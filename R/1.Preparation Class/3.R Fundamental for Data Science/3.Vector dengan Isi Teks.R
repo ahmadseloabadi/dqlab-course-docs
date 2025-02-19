61bc7e8c9921f9900658c9d920a25550867e9691
selo <ahmadseloabadi@gmail.com>
2025-02-19 12:43:35 +0700

    feat : add script

---
+++ R/1.Preparation Class/3.R Fundamental for Data Science/3.Vector dengan Isi Teks.R
@@ -0,0 +1,18 @@
+# Variable nama_mahasiswa dengan input character
+
+nama_mahasiswa <- c("Amira","Budi","Charlie")
+
+print(nama_mahasiswa)
+
+
+# Toeri
+# Selain angka, vector juga bisa diisi dengan teks. 
+
+# Tugas
+# Cobalah ketik satu perintah pada code editor untuk membuat vector yang isinya tiga nama orang dan disimpan sebagai variable  nama_mahasiswa 
+
+# nama_mahasiswa <- c("Amira","Budi","Charlie")
+
+# Penjelasan
+# Terlihat vector yang ditampilkan adalah urutan teks "Amira", "Budi", dan "Charlie". Ini menunjukkan bahwa vector bisa berisi teks selain angka.
+
---
+++ R/1.Preparation Class/3.R Fundamental for Data Science/4.Index dan Accessor pada Vector.R
@@ -0,0 +1,25 @@
+# Buat vector variable bernama angka yang isinya 20 s/d 30
+angka <- c(20:30)
+print(angka)
+
+# Tampilkan isi variable angka pada posisi ke 3
+print(angka[3])
+
+
+# Tampilkan isi variable angka pada posisi ke 5 gunakan kurung siku ganda
+print(angka[[5]])
+
+
+# Tampilkan isi variable angka pada posisi ke 4 s/d 6
+print(angka[4:6])
+
+# Buat vector teks dengan nama kode_prodi yang diisi sesuai petunjuk soal
+kode_prodi <- c("DKV","ILKOM","ICT")
+# Tampilkan isi indeks ketiga dari kode_prodi
+print(kode_prodi[3])
+
+
+# Toeri
+# Untuk mengambil isi vector, kamu dapat mengambil berdasarkan posisinya. Posisi ini diwakili oleh angka urutan, yang disebut sebagai index.
+# Cara penulisan index di variable adalah angka yang diapit dengan kurung siku tunggal atau ganda. Penulisan ini selanjutnya disebut sebagai accessor.
+# Jika index yang ingin diambil lebih dari satu, maka hanya boleh menggunakan kurung siku tunggal.
