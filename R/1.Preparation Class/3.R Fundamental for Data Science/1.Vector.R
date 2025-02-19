ecf9938685dce36fa12d79bc9999a57e11ff5855
selo <ahmadseloabadi@gmail.com>
2025-02-19 12:43:29 +0700

    feat : add script

---
+++ R/1.Preparation Class/3.R Fundamental for Data Science/1.Vector.R
@@ -0,0 +1,12 @@
+# Ini adalah contoh vector untuk angka numerik dengan 3 data c(4, 5, 6)
+c(4, 5, 6)
+
+# Variable bernama angka dengan input berupa vector
+angka <- c(4, 5, 6)
+# Tampilkan isi variable angka dengan fungsi print
+print(angka)
+
+# Teori
+# Vector merupakan tipe data sederhana di R yang menyimpan deretan nilai (lebih dari satu nilai) dengan tipe data sama untuk setiap elemennya. Maksudnya, jika tipe datanya berupa teks maka seluruh elemennya harus bertipe teks. Demikian juga jika tipenya angka maka seluruh elemennya berisi angka semua.
+
+# Vector didefinisikan dengan nama function yang memilki satu huruf saja: c, yang telah kamu pelajari dan praktikkan sebelumnya. Sebagai contoh untuk membuat vector yang isinya angka dengan nilai 2, 5, dan 7, maka perintahnya adalah c(2, 5, 7).
---
+++ R/1.Preparation Class/3.R Fundamental for Data Science/2.Deretan Nilai dengan Operator (Titik Dua).R
@@ -0,0 +1,9 @@
+angka1 <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+print(angka1)
+angka2 <- c(1:10)
+print(angka2)
+
+# Teori
+# Operator ":" atau titik dua merupakan operator yang digunakan untuk mempersingkat penulisan dari nilai-nilai vector yang berurutan. Sebagai contoh, perintah c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10) yang membentuk vector dengan angka 1 sampai dengan 10 dapat disingkat dengan c(1:10).
+
+# Ketiklah empat perintah berikut pada code editor. Variable angka1 diisi dengan vector dengan penulisan angka berurutan yang panjang, dan variable angka2 diisi dengan menggunakan operator ":".
