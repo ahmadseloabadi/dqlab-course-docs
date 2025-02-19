6930b4e74bd2185a21c0c870285796a678c7312c
selo <ahmadseloabadi@gmail.com>
2025-02-19 12:43:41 +0700

    feat : add script

---
+++ R/1.Preparation Class/3.R Fundamental for Data Science/5.Named Vector.R
@@ -0,0 +1,25 @@
+#Membuat named vector dengan nama nilai
+nilai <- c(statistik = 89, fisika = 95, ilmukomunikasi = 100)
+
+#Menampilkan isi variable nilai
+print(nilai)
+
+#Menampilkan isi dengan nama fisika
+print(nilai["fisika"])
+#Buat variable profil sesuai permintaan soal
+profil<-c(nama = "Budi",
+tempat_tinggal = "Jakarta",
+tingkat_pendidikan = "S1")
+#Tampilkan variable profil
+print(profil)
+
+# Teori
+# Selain dengan angka, indeks pada vector juga dapat dilengkapi dengan nama untuk tiap elemennya dengan menggunakan format penulisan name=value. Penggunaan format name=value disebut dengan named vector.
+
+# Tugas
+# Sebagai latihan, agar kamu dapat menguasai bahan named vector ini. Cobalah buat variable named vector profil dengan input berikut:
+
+# nama = "Budi"
+# tempat_tinggal = "Jakarta"
+# tingkat_pendidikan = "S1"
+# Kemudian tampilkan variable profil ini dengan menggunakan fungsi print.
---
+++ R/1.Preparation Class/3.R Fundamental for Data Science/6.List.R
@@ -0,0 +1,36 @@
+# List disimpan dalam variable dengan nama list_random
+list_random <- list(2, "Budi", 4)
+
+# Menampilkan isi list
+list_random 
+
+# List disimpan dalam variable dengan nama dati2
+dati2 <- list(nama = "Denpasar", propinsi = "Bali")
+
+# Menampilkan isi list dati2
+dati2 
+
+# Buat variable kota sesuai permintaan soal
+kota<-list(nama_kota = "Makassar",
+propinsi = "Sulawesi Selatan",
+luas_km2 = 199.3)
+
+# Tampilkan isi variable list kota
+kota
+
+
+# Teori
+# List merupakan jenis data di R yang mirip dengan vector. Perbedaannya adalah list dapat menyimpan lebih dari satu tipe data dasar di setiap elemennya. Untuk memasukkan isi ke dalam struktur data ini kita gunakan function list.
+
+# Tugas Praktik:
+
+# Buatlah variable dengan nama kota dengan isi berikut :
+
+# nama_kota = "Makassar"
+# propinsi = "Sulawesi Selatan"
+# luas_km2 = 199.3
+# Kemudian tampilkan isi variable list kota tersebut.
+
+# penjelasan
+# Terlihat bahwa output-nya agak berbeda dengan vector, di mana tiap output terdapat dua tampilan accessor (kurung siku tunggal dan ganda).
+
