e4ec12a4e63c6ac7f56f2bf9a17f50af0bc45676
selo <ahmadseloabadi@gmail.com>
2025-02-19 12:44:28 +0700

    feat : add script

---
+++ R/2.Fundamental CLass/3.R for Data Professional - Part 3/2.‘Gambaran Menyeluruh’ dari Dataframe yang Digunakan - Praktikum.R
@@ -0,0 +1,12 @@
+#Tampilkan 5 baris pertama dari Dataframe mtcars
+head(mtcars,5)
+
+#Tampilkan 5 baris terakhir dari Dataframe mtcars
+tail(mtcars,5)
+
+# Teori
+# Function head(nama_dataframe, jumlah_baris) dapat digunakan untuk menampilkan beberapa baris awal di Dataframe dan function tail(nama_dataframe, jumlah_baris) dapat digunakan untuk menampilkan beberapa baris terakhir di Dataframe.
+
+# Instruksi :
+
+# Gunakan function head() dan tail() pada Dataframe mtcars untuk menampilkan beberapa baris data pertama dan terakhir dari mtcars.
---
+++ R/2.Fundamental CLass/3.R for Data Professional - Part 3/3.Melihat Struktur dari Dataframe - Praktikum.R
@@ -0,0 +1,17 @@
+#Tampilkan struktur dari mtcars
+str(mtcars)
+
+# Teori
+# Selain menggunakan head() dan tail(), ada cara lain yang sering digunakan untuk mendapatkan gambaran besar dari data. Cara lain yang dapat digunakan adalah dengan function str() yang dapat menunjukkan struktur dari data yang dimiliki. Misalnya, function str() dapat diaplikasikan pada Dataframe mtcars sebelumnya untuk memperoleh beberapa informasi sebagai berikut.
+
+# Total observasi yang ada (32 tipe mobil)
+# Total variable yang dimiliki (11 fitur mobil)
+# Sebuah daftar atau list berisi nama variable (mpg, cyl, dst.)
+# Tipe data dari setiap variable (numeric)
+ 
+
+# Dengan mengaplikasikan function str(), insight mengenai data yang akan digunakan sebelum dianalisis dapat lebih mudah diperoleh. Mengetahui hal ini, aku langsung menerapkan function str() pada Dataframe mtcars.
+
+# Instruksi:
+
+# Aplikasikan function str() pada Dataframe mtcars.
