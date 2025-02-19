70db01ece9dd76c61fd6b923147895dfbc489101
selo <ahmadseloabadi@gmail.com>
2025-02-19 12:43:47 +0700

    feat : add script

---
+++ R/1.Preparation Class/3.R Fundamental for Data Science/7.List Index.R
@@ -0,0 +1,23 @@
+# Membentuk list dengan 2 angka dan 1 character
+list_saya <- list(2, "Budi", 4)
+
+# Menampilkan index kedua dengan aksesor kurung siku tunggal 
+list_saya[2]
+
+# Menampilkan index kedua dengan aksesor kurung siku ganda
+list_saya[[2]]
+
+# Menampilkan index kedua s/d ketiga
+list_saya[2:3]
+
+list_satu<-list(1,'Online',TRUE)
+
+list_satu[1]
+
+
+# Teori
+# Untuk mengambil isi list, kamu dapat mengambil dari posisi index-nya. Ketentuan dan caranya sama persis dengan apa yang telah kamu pelajari di vector.
+
+# Tugas Praktik
+
+# Buatlah variable dengan nama list_satu dengan isi angka 1, character “Online”, dan nilai logical TRUE. Tampilkan index pertama dari list tersebut dengan accessor kurung siku tunggal.
