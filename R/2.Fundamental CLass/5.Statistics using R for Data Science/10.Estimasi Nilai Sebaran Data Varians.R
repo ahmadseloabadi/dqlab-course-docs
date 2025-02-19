380955827e264cd0af549e9bafb63f1052904f7e
selo <ahmadseloabadi@gmail.com>
2025-02-19 17:32:01 +0700

    feat : add script

---
+++ R/2.Fundamental CLass/5.Statistics using R for Data Science/10.Estimasi Nilai Sebaran Data Varians.R
@@ -0,0 +1,19 @@
+## Carilah varians untuk kolom Pendapatan dari variable data_intro
+var(data_intro$Pendapatan)
+
+
+# Teori
+# Varians merupakan simpangan kuadrat data dari nilai rata-ratanya.  Untuk menampilkan varians dari data dapat menggunakan syntax sebagai berikut:
+
+
+
+# dimana
+
+# var adalah function yang digunakan untuk mendapatkan nilai varians dari data.
+
+# Tugas Praktek
+
+# Ganti bagian […1…] dengan perintah untuk menghitung nilai varians kolom Pendapatan dari variabel data_intro.
+
+# penjelasan
+# Keterangan: e+12 menunjukkan 10 pangkat 12. Jadi nilai di atas lengkapnya adalah 1.645.684.210.526.
---
+++ R/2.Fundamental CLass/5.Statistics using R for Data Science/9.Estimasi Nilai Sebaran Data Range.R
@@ -0,0 +1,21 @@
+## carilah range untuk kolom Pendapatan pada variable data_intro
+max(data_intro$Pendapatan) - min(data_intro$Pendapatan)
+
+# Berikut penjelasan function diatas:
+
+# max digunakan untuk mendapatkan nilai maksimal dari data.
+# min adalah function yang digunakan mendapatkan nilai minimal dari data.
+
+
+# Teori
+# Ukuran Sebaran Data
+# Ukuran sebaran yang sering digunakan adalah sebagai berikut :
+
+# Range adalah selisih antara nilai terbesar dan nilai terendah
+# Varians adalah simpangan kuadrat data dari nilai rata-ratanya
+
+# Simpangan baku adalah simpangan data dari nilai rata-ratanya, simpangan baku nama lainnya adalah standard deviation. Standard deviation dapat digunakan untuk melihat keakuratan dari hasil estimasi, semakin kecil standard deviation semakin akurat hasil estimasi.
+
+# Tugas Praktek
+
+# Gantilah bagian […1…] pada code editor dengan perhitungan range dari kolom Pendapatan pada variabel data_intro dengan modifikasi contoh pada Lesson.
