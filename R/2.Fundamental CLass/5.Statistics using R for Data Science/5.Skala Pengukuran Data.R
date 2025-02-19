b0978e04c1427c0be6548739bad0735e95e5acaa
selo <ahmadseloabadi@gmail.com>
2025-02-19 17:31:51 +0700

    feat : add script

---
+++ R/2.Fundamental CLass/5.Statistics using R for Data Science/5.Skala Pengukuran Data.R
@@ -0,0 +1,12 @@
+# melihat data/ pemanggilan data
+data_intro
+# melihat tipe data
+str(data_intro)
+
+
+# Teori
+# Setelah data diubah jenis tipe datanya, selanjutnya adalah pemeriksaan untuk memastikan apakah tipe data setiap variabel sudah sesuai dengan skala pengukuran masing-masing.
+
+# Tugas Praktek
+
+# Lengkapi bagian [...1...] dan [...2...] pada code editor untuk menampilkan variable data_intro dan strukturnya dengan function str.
---
+++ R/2.Fundamental CLass/5.Statistics using R for Data Science/6.Estimasi Nilai Statistik Modus.R
@@ -0,0 +1,23 @@
+library(pracma)
+## carilah modus untuk kolom Produk pada variable data_intro
+Mode(data_intro$Produk)
+## carilah modus untuk kolom Tingkat.Kepuasan pada variable data_intro
+Mode(data_intro$Tingkat.Kepuasan)
+
+
+# Teori
+# Modus merupakan nilai yang menunjukan nilai yang sering muncul. Modus digunakan untuk data bertipe nominal dan ordinal.
+
+# Untuk menampilkan modus dari data dapat menggunakan syntax :
+
+
+
+# Berikut penjelasan function diatas :
+
+# Mode akan menampilkan nilai terbanyak pada variabel yang diamati.
+# data_intro$Produk, merupakan kolom Produk dari variabel data_intro.
+# Untuk menggunakan function Mode tersebut, menggunakan library tambahan bernama "pracma".
+
+# Tugas Praktek
+
+# Lengkapi bagian [...1...] pada code editor untuk  membaca file seperti yang ditunjukkan pada bagian Lesson. Dan lengkapi bagian [...2...] untuk melihat modus pada kolom tingkat kepuasan.
