76342502b07fdfcff2ecbdc2bc273ecae46a43e5
selo <ahmadseloabadi@gmail.com>
2025-02-21 00:13:09 +0700

    feat : add script

---
+++ R/2.Fundamental CLass/7.Data Preparation in Data Science using R/15.Function levels dan class pada Factor.R
@@ -0,0 +1,16 @@
+#Variable factor bernama faktor.bulan dengan nilai teks "Jan", "Feb", dan "Mar"
+faktor.bulan <- factor(c("Jan","Feb","Mar"))
+levels(faktor.bulan)
+class(faktor.bulan)
+
+
+# Teori
+# Atribut levels dan class dapat juga dilihat dengan function levels dan class dengan input berupa factor, dengan konstruksi sebagai berikut:
+
+# levels(variable)
+# class(variable)
+# Mari kita langsung lakukan praktek agar lebih jelas.
+
+# Tugas Praktek
+
+# Pada code editor, terdapat satu variable bernama faktor.bulan yang telah diisi dengan factor. Tampilkan levels dari variable dengan perintah yang menggantikan bagian […1…]. Kemudian menampilkan class dari variable dengan perintah yang menggantikan bagian […2…].
