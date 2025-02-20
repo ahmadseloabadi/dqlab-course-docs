3a3229de4eb51280c6bbbf159443937b4539b4d6
selo <ahmadseloabadi@gmail.com>
2025-02-21 00:13:14 +0700

    feat : add script

---
+++ R/2.Fundamental CLass/7.Data Preparation in Data Science using R/17.Penggunaan as.integer pada Factor.R
@@ -0,0 +1,10 @@
+#Buatlah factor dengan teks "Jan", "Feb", "Mar","Jan","Mar", dan "Jan"
+factor.bulan <- factor(c("Jan","Feb","Mar","Jan","Mar","Jan"))
+as.integer(factor.bulan)
+
+
+# Teori
+# Untuk mengambil nilai-nilai index integer pada factor, kita bisa menggunakan function as.integer dengan syntax berikut.
+
+# as.integer(variable factor)
+# Mari kita langsung praktekkan dengan tugas berikut.
