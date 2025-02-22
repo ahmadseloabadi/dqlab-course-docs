84ca162eb2b1cb4130596541dd5aba58aa32143a
selo <ahmadseloabadi@gmail.com>
2025-02-22 19:57:42 +0700

    feat : add script

---
+++ R/3.Application in Industry Class/1.Data Science in Marketing Customer Segmentation/19.Data Baru.R
@@ -0,0 +1,15 @@
+# Silakan diketikkan code yang dicontohkan dan kemudian hapuslah komentar ini
+databaru <- data.frame(Customer_ID="CUST-100", Nama.Pelanggan="Rudi Wilamar",Umur=20,Jenis.Kelamin="Wanita",Profesi="Pelajar",Tipe.Residen="Cluster",NilaiBelanjaSetahun=3.5)
+databaru
+
+# Teori
+# Pada teks sebelumnya, disebutkan data pelanggan baru harus cepat dipetakan ke segmen.
+
+# Dengan asumsi tiap data pelanggan baru diinput ke dalam sistem, maka pengolahan adalah per record. Pada praktek kali ini, kita akan membuat data.frame dengan satu data dimana nama-nama kolomnya persis dengan dataset awal.
+
+# Tugas Praktek
+
+# Ketik perintah kedua perintah berikut pada code editor, masing-masing mengisi data.frame dan menampilkannya.
+
+# databaru <- data.frame(Customer_ID="CUST-100", Nama.Pelanggan="Rudi Wilamar",Umur=20,Jenis.Kelamin="Wanita",Profesi="Pelajar",Tipe.Residen="Cluster",NilaiBelanjaSetahun=3.5)
+# databaru
---
+++ R/3.Application in Industry Class/1.Data Science in Marketing Customer Segmentation/20.Memuat Objek Clustering dari File.R
@@ -0,0 +1,16 @@
+Identitas.Cluster <- readRDS(file="cluster.rds")
+Identitas.Cluster
+
+# Teori
+# Praktek kali ini adalah membuka file yang telah kita simpan sebelumnya dengan perintah dan dikenali di R sebagai objek yang akan kita gunakan untuk mengolah data baru.
+
+# Untuk membuka file tersebut, kita gunakan function readRDS.
+
+# Perintahnya sangat sederhana, berikut adalah contoh untuk membuka file cluster.rds yang telah kita simpan sebelumnya.
+
+# readRDS(file="cluster.rds")
+# Lakukan tugas berikut untuk membaca dan menampilkan objek dari cluster.rds.
+
+# Tugas Praktek
+
+# Baca objek dari file cluster.rds dengan menggunakan function readRDS, dan simpan sebagai variable dengan nama Identitas.Cluster. Setelah itu, tampilkan isi dari Identitas.Cluster.
