1f90574566978dbf2af444f1316d381c3be9f4bb
selo <ahmadseloabadi@gmail.com>
2025-02-23 21:54:09 +0700

    feat : add script

---
+++ R/3.Application in Industry Class/5.Analisis Data COVID19 di Indonesia/7.Mengekstrak isi Respon.R
@@ -0,0 +1,9 @@
+library(httr)
+set_config(config(ssl_verifypeer = 0L))
+resp <- GET("https://storage.googleapis.com/dqlab-dataset/update.json")
+cov_id_raw <- content(resp, as = "parsed", simplifyVector = TRUE) 
+
+# Teori
+# Respon API dengan status code 200 menyatakan bahwa permintaan Anda berhasil dipenuhi dan konten yang diminta tersedia untuk diekstrak. Selain itu Anda juga telah mengetahui lewat fungsi headers() bahwa konten yang diminta tersedia dalam bentuk application/json, yaitu berkas JSON.
+
+# Selanjutnya Anda dapat mengekstrak konten tersebut dengan menggunakan fungsi content(). Fungsi content() tersebut secara cerdas akan menyesuaikan output sesuai dengan jenis berkas yang tersedia, dalam hal ini adalah berkas JSON. Jalankan fungsi tersebut pada obyek resp dan tambahkan argumen as = "parsed" dan simplifyVector = TRUE. Simpanlah hasilnya sebagai cov_id_raw!
