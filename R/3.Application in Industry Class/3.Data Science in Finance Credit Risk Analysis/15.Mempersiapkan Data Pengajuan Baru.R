b7874e798f2264126599c9933e526bd10540a22a
selo <ahmadseloabadi@gmail.com>
2025-02-23 19:03:56 +0700

    feat : add script

---
+++ R/3.Application in Industry Class/3.Data Science in Finance Credit Risk Analysis/15.Mempersiapkan Data Pengajuan Baru.R
@@ -0,0 +1,13 @@
+#Membuat data frame aplikasi baru
+aplikasi_baru <- data.frame(jumlah_tanggungan = 6, durasi_pinjaman_bulan = 12)
+print(aplikasi_baru)
+
+# Teori
+# Data pengajuan baru perlu dibentuk sebagai satu data frame dengan input dimana nama-nama variable yang digunakan harus sama persis. Dari awal pemodelan, kita menggunakan dua variable yakni:
+
+# jumlah_tanggungan
+# durasi_pinjaman_bulan
+# Keduanya dalam bentuk numerik (angka). Dan berikut adalah contoh membentuk data frame dengan dua variable tersebut.
+
+# aplikasi_baru <- data.frame(jumlah_tanggungan = 6, durasi_pinjaman_bulan = 12)
+# Code tersebut sudah terdapat pada code editor. Namun, Anda perlu tampilkan variable tersebut dengan perintah print. 
