c089ff404aea0f04e962569e3e1e2f8641aa973e
selo <ahmadseloabadi@gmail.com>
2025-02-26 17:42:12 +0700

    feat : add script

---
+++ SQL/3.Projects/1.Data Engineer Challenge with SQL/4.Mengurutkan Nama Pelanggan.sql
@@ -0,0 +1,6 @@
+select nama_pelanggan from ms_pelanggan ORDER BY nama_pelanggan;
+
+-- Tugas
+-- Tampilkan nama-nama pelanggan dan urutkan hasilnya berdasarkan kolom nama_pelanggan dari yang terkecil ke yang terbesar (A ke Z).
+
+-- Nama kolom yang harus ditampilkan: nama_pelanggan.
