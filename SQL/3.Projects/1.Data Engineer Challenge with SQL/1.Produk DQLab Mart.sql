cba37f68998e4345103e7ddab01ecd6edb5d59fc
selo <ahmadseloabadi@gmail.com>
2025-02-26 17:42:04 +0700

    feat : add script

---
+++ SQL/3.Projects/1.Data Engineer Challenge with SQL/1.Produk DQLab Mart.sql
@@ -0,0 +1,10 @@
+SELECT no_urut,kode_produk,nama_produk,harga 
+FROM ms_produk 
+WHERE harga > 50000 AND harga < 150000
+
+-- Tugas
+-- Mengacu pada table ms_produk, tampilkan daftar produk yang memiliki harga antara 50.000 and 150.000.
+
+-- Nama kolom yang harus ditampilkan: no_urut, kode_produk, nama_produk, dan harga.
+
+-- Semua table di atas sudah tersedia, Anda tinggal menulis query Anda dalam Code Editor.
---
+++ SQL/3.Projects/1.Data Engineer Challenge with SQL/2.Thumb drive di DQLab Mart.sql
@@ -0,0 +1,8 @@
+SELECT no_urut,kode_produk,nama_produk,harga
+FROM ms_produk
+WHERE nama_produk LIKE '%Flashdisk%'
+
+-- Tugas
+-- Tampilkan semua produk yang mengandung kata Flashdisk.
+
+-- Nama kolom yang harus ditampilkan: no_urut, kode_produk, nama_produk, dan harga.
