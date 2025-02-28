35ee41664adc627cf2d30ecf2450e424abd89883
selo <ahmadseloabadi@gmail.com>
2025-02-28 23:49:42 +0700

    feat : add script

---
+++ R/4.projects/5.Project Analisa Klasifikasi Pinjaman untuk Sektor UMKM/3.Statistik Dekriptif data.R
@@ -0,0 +1,9 @@
+summary(data)
+
+# Teori
+# Melalui R kita dapat menampilkan statistik deskriptif pada data yang dimiliki. Jika diinginkan lebih spesifik maka dapat dilakukan pada kolom tertentu pada tabel data yang kita punya. Tentunya kita dapat menggunakan accessor $ untuk memilih kolom yang diinginkan dari data.
+# summary(data$OSL)
+
+# Tugas:
+
+# Tentukanlah statistik deskriptif untuk seluruh kolom data, dan apa yang dapat kamu infer?
---
+++ R/4.projects/5.Project Analisa Klasifikasi Pinjaman untuk Sektor UMKM/4.Menghapus Kolom.R
@@ -0,0 +1,11 @@
+data_reduce = data[-c(1,2)]
+colnames(data_reduce)
+
+# Teori
+# Pada data yang kamu miliki, sebenarnya kamu tidak memerlukan nama pelanggan untuk diberikan rekomendasi. Atau dengan kata lain penanda pelanggan untuk diberikan rekomendasi cukup dengan melihat no_kontrak pelanggan itu saja.
+
+ 
+
+# Tugas:
+
+# Hapuslah kolom "X" dan nama nasabah pada data yang kamu miliki dan cetak kembali nama kolom yang tersedia pada data.
