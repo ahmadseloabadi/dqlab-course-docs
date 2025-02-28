73a5bc86f8e396b025b706cdb86086e90530aab3
selo <ahmadseloabadi@gmail.com>
2025-02-28 23:49:37 +0700

    feat : add script

---
+++ R/4.projects/5.Project Analisa Klasifikasi Pinjaman untuk Sektor UMKM/1.Membaca Data External.R
@@ -0,0 +1,5 @@
+data = read.csv("https://storage.googleapis.com/dqlab-dataset/project.csv")
+
+# Tugas
+# Hal yang pertama dilakukan adalah membaca dataset yang telah dipersiapkan. Dengan menggunakan fungsi bawaan R bacalah dataset yang berformat csv tersebut yang bernama "https://storage.googleapis.com/dqlab-dataset/project.csv".
+
---
+++ R/4.projects/5.Project Analisa Klasifikasi Pinjaman untuk Sektor UMKM/2.Inspeksi data.R
@@ -0,0 +1,11 @@
+# Enam baris teratas data
+head(data)
+
+# Tampilkan tipe data setiap kolomnya
+str(data)
+
+# Tugas
+# Setelah data berhasil di import, cobalah kamu untuk menginspeksi dataset dengan jalan
+
+# Melihat 6 baris pertama data tersebut, apa saja yang ditunjukkannya dan
+# Tampilkan tipe data dari setiap kolom.
